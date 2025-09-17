9a0537d9fdd74bf7ec45a4dd40c8deaf014ed546
Victor <victorfernandes1669@gmail.com>
2025-09-17 11:53:43 -0300

    Layout for tablet and desktop devices, altered JSX structure, added better class names, and CSS organization.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -20,140 +20,141 @@ export default function App() {
 
   return (
     <div className="App">
-      <header>
-        <h1 className="logo">Weather Now</h1>
+      <header id="Header">
+        <h1 className="h-logo">Weather Now</h1>
         <div
+          className="h-dropbox generalBackground"
           role="combobox"
           aria-haspopup="listbox"
           aria-expanded="false"
-          className="unitsDropbox generalBackground"
           tabIndex="0"
         >
           <img width={14} src="/images/icon-units.svg" alt="" />
           <span>Units</span>
           <img width={14} src="/images/icon-dropdown.svg" alt="" />
         </div>
-        <h2 className="subtitle">How's the sky looking today?</h2>
+        <h2 className="h-subtitle">How's the sky looking today?</h2>
       </header>
-      <main>
-        <form onSubmit={handleSubmit}>
-          <div role="searchbox" className="searchBar generalBackground">
-            <label className="searchLabel" htmlFor="searchInput">
-              <img src="/images/icon-search.svg" />
-            </label>
-            <input
-              placeholder="Search for a place..."
-              type="text"
-              id="searchInput"
-            />
-          </div>
-          <button className="searchButton">Search</button>
-        </form>
-        <section className="sectionLocation">
-          <div className="locationDataContainer">
+      <div id="SearchBar" className="generalBackground" role="searchbox">
+        <label className="s-label" htmlFor="SearchInput">
+          <img src="/images/icon-search.svg" />
+        </label>
+        <input
+          className="s-input"
+          placeholder="Search for a place..."
+          type="text"
+          id="SearchInput"
+        />
+      </div>
+      <button id="SearchButton" onClick={handleSubmit}>
+        Search
+      </button>
+      <main id="Main">
+        <section id="m-1-location-forecast">
+          <div className="m-1-container">
             <header>
-              <h3>Location</h3>
+              <h3 className="m-1-location-name">Location</h3>
               <p>Date</p>
             </header>
-            <div className="locationClimateData" role="complementary">
+            <div className="m-1-container-data" role="complementary">
               <img
                 width={100}
                 src={ClimateImages[7]?.image}
                 alt={ClimateImages[7]?.alt}
               />
-              <span>NUM</span>
+              <span className="m-1-data-number">NUM</span>
             </div>
           </div>
-          <ul className="locationDataListComplementary" role="group">
-            <li className="locationData generalBackground">
+          <ul id="m-1-list" role="group">
+            <li className="m-1-data generalBackground">
               <span>[TITLE]</span> <span>[DATA]</span>
             </li>
-            <li className="locationData generalBackground">
+            <li className="m-1-data generalBackground">
               <span>[TITLE]</span> <span>[DATA]</span>
             </li>
-            <li className="locationData generalBackground">
+            <li className="m-1-data generalBackground">
               <span>[TITLE]</span> <span>[DATA]</span>
             </li>
-            <li className="locationData generalBackground">
+            <li className="m-1-data generalBackground">
               <span>[TITLE]</span> <span>[DATA]</span>
             </li>
           </ul>
         </section>
-        <section className="dailyForecast">
+        <section id="m-2-daily-forecast">
           <h3>Daily forecast</h3>
-          <ul className="dailyListForecast" role="group">
-            <li className="dailyData generalBackground">
+          <ul id="m-2-list" role="group">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
-            <li className="dailyData generalBackground">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
-            <li className="dailyData generalBackground">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
-            <li className="dailyData generalBackground">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
-            <li className="dailyData generalBackground">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
-            <li className="dailyData generalBackground">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
-            <li className="dailyData generalBackground">
+            <li className="m-2-data generalBackground">
               [DAY] <figure>[IMAGE]</figure>
-              <span className="minMax">[MIN] [MAX]</span>
+              <span className="m-2-min-max">[MIN] [MAX]</span>
             </li>
           </ul>
         </section>
-        <aside className="hourlyForecast generalBackground">
-          <header className="hourlyHeader">
-            <h3>Hourly forecast</h3>
-            <div
-              role="combobox"
-              aria-haspopup="listbox"
-              aria-expanded="false"
-              className="daysDropbox"
-              tabIndex="0"
-            >
-              <span>{day}</span>
-              <img width={14} src="/images/icon-dropdown.svg" alt="" />
-            </div>
-          </header>
-          <ul className="hourlyListForecast" role="group">
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-            <li className="hourlyData">
-              [IMAGE] [HOUR] <span>[DEGREE]</span>
-            </li>
-          </ul>
-        </aside>
       </main>
+      <aside id="Aside" className="generalBackground">
+        <header className="a-header">
+          <h3>Hourly forecast</h3>
+          <div
+            className="a-dropbox"
+            role="combobox"
+            aria-haspopup="listbox"
+            aria-expanded="false"
+            tabIndex="0"
+          >
+            <span>{day}</span>
+            <img width={14} src="/images/icon-dropdown.svg" alt="" />
+          </div>
+        </header>
+        <ul role="group">
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+          <li className="a-data">
+            [IMAGE] [HOUR] <span>[DEGREE]</span>
+          </li>
+        </ul>
+      </aside>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { createRoot } from "react-dom/client";
 
 import App from "./Components/App.jsx";
 
-import "./Stylesheets/aside.css";
+import "./Stylesheets/grid.css";
 import "./Stylesheets/main.css";
+import "./Stylesheets/index.css";
+
 import "./Stylesheets/search-bar.css";
 import "./Stylesheets/header.css";
-import "./Stylesheets/fonts.css";
-import "./Stylesheets/index.css";
+import "./Stylesheets/aside.css";
 
 createRoot(document.getElementById("root")).render(<App />);
